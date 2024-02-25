9c9e01e10fe892c75c6775ff165e069ca472c44c
Aaron1376 <Aaronsam1376@gmail.com>
2024-02-24 17:50:28 -0800

    Pushing PA2

---
+++ src/PA2Search.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class PA2Search {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int search_terms;
+        String search_file;
+
+        search_terms = Integer.parseInt(args[0]);
+        search_file = args[1];
+
+
+
+    }
+}
